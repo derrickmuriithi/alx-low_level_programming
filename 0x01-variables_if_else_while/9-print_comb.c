96d2bf9b8d155f823f69d7db2c0fef041916ce42
derrickmuriithi <muriithiderrick6@gmail.com>
2022-12-30 12:28:15 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int digit;
+for (digit = 48; digit != 58; digit++)
+{
+putchar(digit);
+if (digit != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+}
