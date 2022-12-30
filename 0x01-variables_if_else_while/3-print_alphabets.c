c45b28d7c609627720c21d1b0f8dfb4853a97844
derrickmuriithi <muriithiderrick6@gmail.com>
2022-12-30 11:26:00 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char lower = 'a';
+char upper = 'A';
+for (lower = 'a'; lower != '{'; lower++)
+putchar(lower);
+for (upper = 'A'; upper != '['; upper++)
+putchar(upper);
+putchar('\n');
+return (0);
+}
