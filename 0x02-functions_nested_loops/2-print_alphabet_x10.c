da9fb1ba7ee1a59e9c79a34bab0b8b4c6428ff9e
derrickmuriithi <muriithiderrick6@gmail.com>
2022-12-30 16:27:00 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints the alphabet 10 times, in lowercasie
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+int i;
+char c;
+for (i = 0; i <= 9; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
