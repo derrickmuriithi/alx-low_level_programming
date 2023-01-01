903d56a201503076cfe67946a65f107407645278
derrickmuriithi <muriithiderrick6@gmail.com>
2023-01-01 09:14:06 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include "main.h"
+/**
+* _isupper - checks for uppercase character
+* @c: character (integer)
+*
+* Return: 1 if c is uppercase & 0 otherwise
+*/
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+return (0);
+}
