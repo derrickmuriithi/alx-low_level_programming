ce2671312d4630e8631afa2c278ac9f4fff2accd
derrickmuriithi <114956335+derrickmuriithi@users.noreply.github.com>
2023-07-18 03:19:11 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,22 @@
+
 #include "dog.h"
+
+/**
+ * init_dog - initialize a varibale of type struct dog
+ * @d: dog structure
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: void
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d != NULL)
-    {
-        d->name = name;
-        d->age = age;
-        d->owner = owner;
-    }
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
-
